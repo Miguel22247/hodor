473886c17d5674090c1720f2b9e38e3afc494892
Miguel Pacheco <minipachru@gmail.com>
2021-05-30 16:11:34 -0300

    updated code

--- level_0/level0.py
+++ level_0/level0.py
@@ -4,12 +4,14 @@
 """Hodor with my Holberton ID 1024 times."""
 import requests
 
-php = "http://158.69.76.135/level0.php"
-vote = {
-    "id": "2780",
-    "holdthedoor": "Submit"
-}
+success_votes = 0
+u_id = int(input("Please write your Holberton ID: ")
+vote_print = 1024
+vote = {'id' : user_id, 'holdthedoor': 'Submit')
+        
+        for i in range (0, vote_print)
+            r = request.post('http://158.69.76.135/level0.php', data=votation)
+            if r.status_id == 200:
+                success_votes +=1
 
-if __name__ == "__main__":
-    for i in range(0, 1024):
-        requests.post(php, data=vote)
+        print("success: {}".format(success_votes))
