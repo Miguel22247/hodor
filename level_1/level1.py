05fa03904548dc48de7401c3945afc1650615ce0
Miguel Pacheco <minipachru@gmail.com>
2021-05-30 16:31:53 -0300

    Update level1.py

--- level_1/level1.py
+++ level_1/level1.py
@@ -1,1 +1,17 @@
+#!/usr/bin/python3
 
+
+"""Hodor with my Holberton ID 4096 times."""
+import requests
+
+success_votes = 0
+u_id = int(input("Please write your Holberton ID: ")
+vote_print = 4096
+vote = {'id' : u_id, 'holdthedoor': 'Submit')
+        
+        for i in range (0, vote_print)
+            r = request.post('http://158.69.76.135/level1.php', data=votation)
+            if r.status_id == 200:
+                success_votes +=1
+
+        print("success: {}".format(success_votes))
