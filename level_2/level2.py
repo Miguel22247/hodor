216938111ddc0612d4d6a8b8886aeacdaeefbf89
Miguel22247 <nr2862002@gmail.com>
2021-05-31 18:29:02 -0300

    reduced in lines

--- level_2/level2.py
+++ level_2/level2.py
@@ -5,5 +5,4 @@
 import requests
 
 php = "http://158.69.76.135/level2.php"
-user_agent = ("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:64.0) "
-              "Gecko/20100101 Firefox/64.0")
+user_agent = ("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:64.0) ""Gecko/20100101 Firefox/64.0")
