02defe9e5f7722555944a4707b506c4cfd984ea7
Miguel22247 <nr2862002@gmail.com>
2021-05-31 13:03:13 -0300

    rewrited from scratch

--- level_0/level0.py
+++ level_0/level0.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
-
-
-"""Hodor with my Holberton ID 1024 times."""
+ 
+ 
+"""hodor for level 0"""
 import requests
 
-success_votes = 0
-u_id = int(input("Please write your Holberton ID: ")
-vote_print = 1024
-vote = {'id' : u_id, 'holdthedoor': 'Submit')
-        
-        for i in range (0, vote_print)
-            r = request.post('http://158.69.76.135/level0.php', data=votation)
-            if r.status_id == 200:
-                success_votes +=1
+page = "http://158.69.76.135/level0.php"
+payloader = {'id': '2780', 'holdthedoor': 'Enviar'}
 
-        print("success: {}".format(success_votes))
+for i in range (0, 1024):
+    loader = request.post(x, data = payloader)
+    print("vote n* {:d} Code: {}".format(i, loader))
