96602b82b1ca305bef7ea61d47fba9ab47e36b3a
Miguel22247 <nr2862002@gmail.com>
2021-05-31 18:24:12 -0300

    fixed?

--- level_2/level2.py
+++ level_2/level2.py
@@ -3,7 +3,7 @@
  
 """hodor for level 2"""
 import requests
-from bs4 import BeautifulSoup
+
 
 page = "http://158.69.76.135/level2.php/"
 os = ("Mozilla/5.0 (Windows NT 10.0; Win64: x64; rv: 64.0) ""Gecko/20100101 Firefox/64.0")
