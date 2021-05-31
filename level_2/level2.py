bbfba90d16be5dbf0f4e8701d5cc7e3efdd36891
Miguel22247 <nr2862002@gmail.com>
2021-05-31 19:55:06 -0300

    fixed thing

--- level_2/level2.py
+++ level_2/level2.py
@@ -5,14 +5,14 @@
 import requests
 
 page = "http://158.69.76.135/level2.php"
-Os = ("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:64.0) ""Gecko/20100101 Firefox/64.0")
+os = ("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:64.0) ""Gecko/20100101 Firefox/64.0")
+
 
 for i in range(0, 1024):
-	get = requests.get(page, Os)
+	get = requests.get(page)
 	cookies = get.cookies
 	dictionary = cookies.get_dict()
 	key = dictionary.get('HoldTheDoor')
 	payloader = {'id': '2780', 'key': key, 'holdthedoor': 'Enviar'}
-	result = requests.post(page, Os, data=payloader, cookies = cookies)
+	result = requests.post(page, os, data=payloader, cookies = cookies)
 	print("Vote n*{:d} send it with code {}".format(i, result, key))
-
